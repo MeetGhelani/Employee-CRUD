018198b57acfe5d104da82e1a42793a2c19d2852
Meet Ghelani <meetghelani10@gmail.com>
2026-06-03 12:01:13 +0530

    Polished UI components

--- EmployeeAPI/DTOs/CreateEmployeeDto.cs
+++ EmployeeAPI/DTOs/CreateEmployeeDto.cs
@@ -5,12 +5,15 @@ namespace EmployeeAPI.DTOs;
 public class CreateEmployeeDto
 {
     [Required]
+    [StringLength(100)]
     public string Name { get; set; } = string.Empty;
 
     [Required]
     [EmailAddress]
+    [StringLength(150)]
     public string Email { get; set; } = string.Empty;
 
     [Required]
+    [StringLength(100)]
     public string Department { get; set; } = string.Empty;
 }
